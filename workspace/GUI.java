972ab56eb0d455cfd0b0d286af871de0a058dc2f
Mr. M <77518360+menchukf@users.noreply.github.com>
2024-10-14 16:13:30 -0400

    Add files via upload

---
+++ workspace/GUI.java
@@ -0,0 +1,96 @@
+package solitaire;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import solitaire.Card.Suit;
+
+public class GUI extends JFrame implements ActionListener, MouseListener, MouseMotionListener{
+
+	Solitaire game;
+   public GUI(Solitaire game){
+	   this.game= game;
+        //Create and set up the window.
+       setTitle("Solitaire");
+       setSize(900,700);
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
+       // this supplies the background
+       try {
+    	   URL background = getClass().getResource("../solitaire/images/background.jpg");
+    	   setContentPane(new ImagePanel(ImageIO.read(background)));
+       }catch(IOException e) {
+    	   e.printStackTrace();
+       }
+       
+       /*******
+        * This is just a test to make sure images are being read correctly on your machine. Please replace
+        * once you have confirmed that the card shows up properly. The code below should allow you to play the solitare
+        * game once it's fully created.
+        */
+       Card card = new Card(2, Suit.Diamonds);
+       System.out.println(card);
+       this.add(card);    
+
+        this.setVisible(true);
+    }
+
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
